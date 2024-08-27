72236047e538da28aba833447da6a9cf71475566
tanvir_rahman__ <asm.hr355279@gmail.com>
2024-08-27 22:44:50 +0600

    Add__ complete design ready for deployment

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Lusitana, Outfit } from 'next/font/google'
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
+
+export const outfit = Outfit({ subsets: ['latin'] })
