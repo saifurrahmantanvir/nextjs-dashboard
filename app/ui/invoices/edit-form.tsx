427b7b55834fc22701291f5f55e1afdd2ada5298
tanvir_rahman__ <asm.hr355279@gmail.com>
2024-09-01 15:11:28 +0600

    Add__ error handling and form validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { State, updateInvoice } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -20,8 +21,13 @@ export default function EditInvoiceForm({
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
+  const initialState: State = { message: null, errors: {} }
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+
+  console.log(state);
+
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
